677b8677503b6a7825d247c5220323173c358b0a
jean0405 <jeanpierrea0405@gmail.com>
2023-08-15 09:10:09 -0500

    feat: :sparkles: Se agregan endpoints en las rutas
    Se han agregado los endpoints para BODEGAS y PRODUCTOS donde se realizan POST y GET

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,9 +1,42 @@
 import { Router } from "express";
+import { connDB } from "../../db/connectDB.js";
+import { limitRequest } from "../helpers/limitRequest.js";
 
 const PRODUCTOS = Router();
 
-PRODUCTOS.get("/", async (req, res) => {
-  res.send("products done!");
+PRODUCTOS.get("/", limitRequest(), async (req, res) => {
+  try {
+    let db = await connDB();
+    let coleccion = db.collection("products");
+    let data = await coleccion.find().toArray();
+    res
+      .status(200)
+      .send({ status: 200, message: "DATOS OBTENIDOS CORRECTAMENTE", data });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      errorInfo: {
+        message: "ERROR AL OBTENER LOS DATOS",
+        error: error.message,
+      },
+    });
+  }
+});
+
+PRODUCTOS.post("/", limitRequest(), async (req, res) => {
+  try {
+    let db = await connDB();
+    let coleccion = db.collection("products");
+    await coleccion.insertOne(req.body);
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      errorInfo: {
+        message: "ERROR AL INSERTAR LOS DATOS",
+        error: error.message,
+      },
+    });
+  }
 });
 
 export default PRODUCTOS;
