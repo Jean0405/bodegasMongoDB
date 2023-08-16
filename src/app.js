db3861147102855b4df7f261fcbaf1f70b758127
Jean0405 <jeanpierrea0405@gmail.com>
2023-08-15 20:40:01 -0500

    feat: :sparkles: Se agregan módulos de generar token
    Se crea la ruta para generar token

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
+console.clear();
 import dotenv from "dotenv";
 import express from "express";
 // import { SERVER } from "./config/configServer.js";
 import BODEGAS from "./routers/bodegasRouter.js";
-import PRODUCTOS from "./routers/productRouter.js";
+import PRODUCTOS from "./routers/productoRouter.js";
+import { generateToken, verifyToken } from "./jwt/tokens.js";
+import { limitRequest } from "./helpers/limitRequest.js";
 
 dotenv.config();
 const SERVER = JSON.parse(process.env.CONFIG_SERVER);
 const APP = express();
 APP.use(express.json());
 
-APP.use("/bodegas", BODEGAS);
-APP.use("/productos", PRODUCTOS);
+APP.use("/token", limitRequest(), generateToken);
+APP.use("/bodegas", limitRequest(), verifyToken, BODEGAS);
+APP.use("/productos", limitRequest(), verifyToken, PRODUCTOS);
 
 APP.listen(SERVER, () =>
   console.log(`http://${SERVER.hostname}:${SERVER.port}`)
